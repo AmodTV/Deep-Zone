1ae4f337bb150138c8c7e3ac3159faf225b543c5
AmodTV <amodtv.1999@gmail.com>
2022-07-25 06:40:35 +0530

    Rough structure of timer built

--- JS/Code.js
+++ JS/Code.js
@@ -1,6 +1,46 @@
 
-var x, s = 0, m = 25, stopS = 0, stopM = 25, started = false;
+var x, started = false, s, m, sec, min, stopS, stopM, workSec, workMin, breakSec, breakMin;
+function done() {
+    var select = document.getElementById("timeOpt");
+    var text = select.options[select.selectedIndex].text;
+    console.log(text);
+    if (text === "25/5") {
+        workSec = 0;
+        workMin = 25;
+        breakSec = 10;
+        breakMin = 0;
+    }
+    else {
+        workSec = 0;
+        workMin = 50;
+        breakSec = 0;
+        breakMin = 10;
+    }
+}
+function workTime() {
+    console.log("Work", "sec = ", sec, "min = ", min);
+    clearInterval(x);
+    s = workSec;
+    m = workMin;
+    stopS = workSec;
+    stopM = workMin;
+    sec = workSec;
+    min = workMin;
+    console.log("Work", "sec = ", sec, "min = ", min);
+}
+function breakTime() {
+    console.log("Break", "sec = ", sec, "min = ", min);
+    clearInterval(x);
+    s = breakSec;
+    m = breakMin;
+    stopS = breakSec;
+    stopM = breakMin;
+    sec = breakSec;
+    min = breakMin;
+    console.log("Break", "sec = ", sec, "min = ", min);
+}
 function start() {
+    console.log(s, m, stopS, stopM, typeof s);
     started = true;
     clearInterval(x);
     s = stopS;
@@ -10,20 +50,24 @@ function start() {
             m--;
             s = 59;
         }
-        if (m == 0 && s == 0) {
-            window.alert("Time up!!");
+        if (m < 0) {
+            document.getElementById('xyz').play();
+            clearInterval(x);
+            m = '00';
+            s = '00';
+            alert("Thank you!");
         }
         document.getElementById("timer")
             .innerHTML = (m) + ':' + (s--);
-        console.log(a);
     }, 1000);
 }
 
 function reset() {
+    console.log(started);
     if (started) {
         clearInterval(x);
-        stopS = 0;
-        stopM = 25;
+        stopS = sec;
+        stopM = min;
         start();
     }
 }
